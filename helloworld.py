5b4cf703b05fa59516b5744fdd3bc31cdab38c13
Srdjan Rados <srdjan.rados90@gmail.com>
2020-12-17 16:02:34 +0100

    json csv

--- helloworld.py
+++ helloworld.py
@@ -99,22 +99,87 @@
 # print(lista[::3])
 
 
-x=0
-lozinka='qwerty'
-unos=input('unesi lozinku: ')
-brojPokusaja=2
-for x in range(brojPokusaja):
-    if unos != lozinka:
-        print('pogresno uneto unesi ponovo')
-        unos = input('unesi lozinku: ')
-    elif unos == lozinka:
-        sum = 0
-        for i in range(1, 101):
-            sum += i
-        print('zbir prvih 100',sum)
-        quit()
-    else:
-        print('Potrosili ste pokusaje')
+# x=0
+# lozinka='qwerty'
+# unos=input('unesi lozinku: ')
+# brojPokusaja=2
+# for x in range(brojPokusaja):
+#     if unos != lozinka:
+#         print('pogresno uneto unesi ponovo')
+#         unos = input('unesi lozinku: ')
+#     elif unos == lozinka:
+#         sum = 0
+#         for i in range(1, 101):
+#             sum += i
+#         print('zbir prvih 100',sum)
+#         quit()
+#     else:
+#         print('Potrosili ste pokusaje')
+
+# numElemetent=int(input('How many items you want: '))
+# lista=[]
+# for i in range(numElemetent):
+#     typeNum=input('What type you want: s f i?')
+#     elem=input('type:')
+#     if typeNum =='s':
+#         lista.append(elem)
+#     elif typeNum =='f':
+#         lista.append(float(elem))
+#     elif typeNum =='i':
+#         lista.append(int(elem))
+# print(lista)
+
+import json
+
+# dictPersonJSON = '{ "name" : "Marko", '\
+#                 ' "surname" : "Markovic", '\
+#                 ' "age":33 '\
+#                 '}'
+# dictPerson = json.loads(dictPersonJSON)
+# print(dictPerson["surname"])
+
+# dictPerson = { "name" : "Marko",
+#                 "surname" : "Markovic",
+#                 "age":33
+#                 }
+#
+# dictPersonJSON = json.dumps(dictPerson)
+# print(dictPersonJSON)
+# print(dictPerson["name"])
+
+
+# import requests
+# req = requests.get("http://api.openweathermap.org/data/2.5/weather?q=Belgrade&APPID=82705ba5bdef2e420e9b9a82d4f11347")
+# cont=req.json()
+# lon=cont["coord"]["lon"]
+# print(lon)
+# desc=cont["weather"][0]["description"]
+# print(desc)
+#
+# print(cont)
+
+import csv
+
+csvFile= open("csvFile.csv")
+readCsv=csv.reader(csvFile, delimiter=",")
+for i in readCsv:
+    print(str.format("Name: {}, Surname: {}, Subject: {}", i[0],i[1],i[2]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
